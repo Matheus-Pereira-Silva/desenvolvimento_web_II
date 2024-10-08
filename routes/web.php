aaf7743e08021796797331f1efd3a8c90625bdfd
Matheus-Pereira-Silva <matheuspereiradasilva08@gmail.com>
2024-10-08 20:48:44 -0300

    teste

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Rota para exibir todas as postagens
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
+
+// Rota para exibir uma postagem específica
+Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+
+// Rotas para comentários
+Route::post('/posts/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
+Route::delete('/comments/{id}', [CommentController::class, 'destroy'])->name('comments.destroy');
